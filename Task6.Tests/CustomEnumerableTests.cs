cd9876096aaa3ce53f600af035950b5a1a80a4b1
crocomoth <justletmeplaymf@gmail.com>
2018-05-04 12:54:04 +0300

    fixed tests

--- Task6.Tests/CustomEnumerableTests.cs
+++ Task6.Tests/CustomEnumerableTests.cs
@@ -32,7 +32,7 @@ namespace Task6.Tests
         {
             ISequenceGenerator<int> generator = new SequenceGenerator<int>();
 
-            var list = generator.GenerateSequence(10, 2, 1, (x, y) => 6 * x - 8 * y);
+            var list = generator.GenerateSequence(10, 1, 2, (x, y) => 6 * y - 8 * x);
 
             int[] actual = list.ToArray();
 
@@ -44,9 +44,18 @@ namespace Task6.Tests
         [Test]
         public void Generator_ForSequence3()
         {
+            ISequenceGenerator<double> sequenceGenerator = new SequenceGenerator<double>();
+
+            var list = sequenceGenerator.GenerateSequence(10, 1, 2, ((d, d1) => d1 + d / d1));
+
+            var actual = list.ToArray();
+
             double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
 
-            Assert.Inconclusive();
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i],actual[i],0.000001);
+            }
         }
     }
 }
